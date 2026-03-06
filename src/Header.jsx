822fd1b289e3f2660a74114d62c8c11ad0b42f68
acolburn <acolburn3@gmail.com>
2026-03-06 14:48:37 -0800

    File paths

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,11 @@
+import myLogo from "./assets/logo-removebg.png";
+
 export default function Header() {
   return (
     <>
       <header>
         <div className="header-logo">
-          <img src="./src/assets/logo-removebg.png" alt="logo" />
+          <img src={myLogo} />
         </div>
         <h1>Alan Colburn</h1>
         <h2>Wannabe Front End Developer</h2>
