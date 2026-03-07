ca87d91479ddb9dd86002f3ffdd4a2a285702b8b
acolburn <acolburn3@gmail.com>
2026-03-07 15:30:19 -0800

    Moved images to public folder

--- src/AppList.jsx
+++ src/AppList.jsx
@@ -1,8 +1,3 @@
-const getImageUrl = (path) => {
-  // This creates a valid URL for assets inside the src folder
-  return new URL(`${path}`, import.meta.url).href;
-};
-
 export default function AppList(props) {
   const appEntry = props.data.map((item) => {
     return (
@@ -12,7 +7,7 @@ export default function AppList(props) {
         <a href={item.url} target="_blank">
           {item.name} URL
         </a>
-        <img src={getImageUrl(item.screenshot)} alt={item.name} />
+        <img src={item.screenshot} alt={item.name} />
       </div>
     );
   });
--- src/myApps.js
+++ src/myApps.js
@@ -5,7 +5,7 @@ export default [
     description:
       "An online two-player version of the classic game. It's made with HTML, CSS, and JavaScript, with a Firebase realtime database backend.",
     url: "https://acolburn.github.io/sequence-js",
-    screenshot: "./assets/sequence_screenshot.png",
+    screenshot: "/assets/sequence_screenshot.png",
   },
   {
     key: "2",
@@ -13,7 +13,7 @@ export default [
     description:
       "I use this app to keep track of my reading. It's made with HTML, CSS, and JavaScript, with a Firebase realtime database backend. Book information is gathered from the Google Books API.",
     url: "https://acolburn.github.io/reading-log",
-    screenshot: "./assets/books_screenshot.png",
+    screenshot: "/assets/books_screenshot.png",
   },
   {
     key: "3",
@@ -21,7 +21,7 @@ export default [
     description:
       "This app lets users calculate how fast they're reading (paper books). It's made with HTML, CSS, and JavaScript.",
     url: "https://acolburn.github.io/reading-speed",
-    screenshot: "./assets/reading_speed_screenshot.png",
+    screenshot: "/assets/reading_speed_screenshot.png",
   },
   {
     key: "4",
@@ -29,7 +29,7 @@ export default [
     description:
       "This app presents a Boggle game board that lets multiple people play together on the web. It creates new boards, keeps track of time, and ends the game when time is up. Players keep track of their words and scores. It's made with Flutter.",
     url: "https://acolburn.github.io/boggle",
-    screenshot: "./assets/boggle2_screenshot.png",
+    screenshot: "/assets/boggle2_screenshot.png",
   },
   {
     key: "5",
@@ -37,7 +37,7 @@ export default [
     description:
       "This version of Boggle allows multiple people to play the same board, at different times. Unlike the previous version, players enter their words online and the game takes care of scoring. It's made with Anvil Python.",
     url: "https://classic-distorted-village.anvil.app",
-    screenshot: "./assets/boggle2_screenshot.png",
+    screenshot: "/assets/boggle2_screenshot.png",
   },
   {
     key: "6",
@@ -45,7 +45,7 @@ export default [
     description:
       "This app presents a Wordle game where every word is the name of a character in a TV show Laura and I have watched. It's made with Flutter.",
     url: "https://acolburn.github.io/wordle",
-    screenshot: "./assets/tv_wordle_screenshot.png",
+    screenshot: "/assets/tv_wordle_screenshot.png",
   },
   {
     key: "7",
@@ -53,7 +53,7 @@ export default [
     description:
       "This app helps users solve the NY Times Spelling Bee game. It's made with Anvil Python.",
     url: "https://tinyurl.com/AlsBeeSolver",
-    screenshot: "./assets/bee_solver_screenshot.png",
+    screenshot: "/assets/bee_solver_screenshot.png",
   },
   {
     key: "8",
@@ -61,7 +61,7 @@ export default [
     description:
       "This app is more or less a clone of simple notetaking apps like Simplenote. I made it to help me keep track of my notes while learning Flutter. It's made with Flutter, with a Google Sheets backend.",
     url: "https://acolburn.github.io/notes-web/",
-    screenshot: "./assets/notes_app_screenshot.png",
+    screenshot: "/assets/notes_app_screenshot.png",
   },
   {
     key: "9",
@@ -69,6 +69,6 @@ export default [
     description:
       "This app was one of the independent exercises in the Scrimba Frontend Developer path. It's made with HTML, CSS, and JavaScript, accessing a public API.",
     url: "https://acolburn.github.io/movie-watchlist/",
-    screenshot: "./assets/movie_watchlist_screenshot.png",
+    screenshot: "/assets/movie_watchlist_screenshot.png",
   },
 ];
