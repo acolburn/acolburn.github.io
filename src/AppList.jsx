b05943c2470ae223255a80b51eff366864e2744b
acolburn <acolburn3@gmail.com>
2026-03-07 15:17:50 -0800

    Added screenshots

--- src/AppList.jsx
+++ src/AppList.jsx
@@ -1,3 +1,8 @@
+const getImageUrl = (path) => {
+  // This creates a valid URL for assets inside the src folder
+  return new URL(`${path}`, import.meta.url).href;
+};
+
 export default function AppList(props) {
   const appEntry = props.data.map((item) => {
     return (
@@ -7,6 +12,7 @@ export default function AppList(props) {
         <a href={item.url} target="_blank">
           {item.name} URL
         </a>
+        <img src={getImageUrl(item.screenshot)} alt={item.name} />
       </div>
     );
   });
