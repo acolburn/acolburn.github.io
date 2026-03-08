a15c5b4315613f5ac7b471f7c013e0ebd43eba79
acolburn <acolburn3@gmail.com>
2026-03-07 16:46:26 -0800

    Fixed height issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,14 @@ import AppList from "./AppList.jsx";
 import myApps from "./myApps.js";
 import Footer from "./Footer.jsx";
 function App() {
-  // 1. Create the state (starting as false/hidden)
   const [showApps, setShowApps] = useState(false);
-  // 2. Create a function to flip that state
+
   const handleToggle = () => setShowApps(!showApps);
 
   return (
     <>
       <Header />
-      {/* 3. Pass the function to Hero as a prop  */}
       <Hero onToggleApps={handleToggle} isShowing={showApps} />
-      {/* We keep the component rendered, but wrap it in a div 
-          that toggles the 'show' class 
-      */}
       <div className={`app-list-container ${showApps ? "show" : ""}`}>
         <AppList data={myApps} />
       </div>
