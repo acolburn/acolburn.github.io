8905fa167dae9c28e0e072c73d04bd884f17d9e2
acolburn <acolburn3@gmail.com>
2026-03-16 15:06:06 -0700

    Added sommClaude

--- src/myApps.js
+++ src/myApps.js
@@ -1,6 +1,14 @@
 export default [
   {
-    key: "1",
+    key: "11",
+    name: "Somm Claude",
+    description:
+      "Let Somm Claude find a great wine pairing for your next meal. Tt's a React app that uses AI.",
+    url: "https://sommclaude.netlify.app",
+    screenshot: "/assets/somm_claude_screenshot.png",
+  },
+  {
+    key: "10",
     name: "Chef Claude",
     description:
       "Give Chef Claude a list of ingredients you have on hand, and he'll suggest a recipe. Tt's a React app that uses AI.",
@@ -8,7 +16,7 @@ export default [
     screenshot: "/assets/chef_claude_screenshot.png",
   },
   {
-    key: "2",
+    key: "9",
     name: "Sequence",
     description:
       "An online two-player version of the classic game. It's made with HTML, CSS, and JavaScript, with a Firebase realtime database backend.",
@@ -16,7 +24,7 @@ export default [
     screenshot: "/assets/sequence_screenshot.png",
   },
   {
-    key: "3",
+    key: "8",
     name: "Reading Log",
     description:
       "I use this app to keep track of my reading. It's made with HTML, CSS, and JavaScript, with a Firebase realtime database backend. Book information is gathered from the Google Books API.",
@@ -24,7 +32,7 @@ export default [
     screenshot: "/assets/books_screenshot.png",
   },
   {
-    key: "4",
+    key: "7",
     name: "Reading Speed",
     description:
       "This app lets users calculate how fast they're reading (paper books). It's made with HTML, CSS, and JavaScript.",
@@ -32,7 +40,7 @@ export default [
     screenshot: "/assets/reading_speed_screenshot.png",
   },
   {
-    key: "5",
+    key: "6",
     name: "Boggle",
     description:
       "This app presents a Boggle game board that lets multiple people play together on the web. It creates new boards, keeps track of time, and ends the game when time is up. Players keep track of their words and scores. It's made with Flutter.",
@@ -40,7 +48,7 @@ export default [
     screenshot: "/assets/boggle1_screenshot.png",
   },
   {
-    key: "6",
+    key: "5",
     name: "Another Boggle Game",
     description:
       "This version of Boggle allows multiple people to play the same board, at different times. Unlike the previous version, players enter their words online and the game takes care of scoring. It's made with Anvil Python.",
@@ -48,7 +56,7 @@ export default [
     screenshot: "/assets/boggle2_screenshot.png",
   },
   {
-    key: "7",
+    key: "4",
     name: "tv Wordle",
     description:
       "This app presents a Wordle game where every word is the name of a character in a TV show Laura and I have watched. It's made with Flutter.",
@@ -56,7 +64,7 @@ export default [
     screenshot: "/assets/tv_wordle_screenshot.png",
   },
   {
-    key: "8",
+    key: "3",
     name: "Bee Solver",
     description:
       "This app helps users solve the NY Times Spelling Bee game. It's made with Anvil Python.",
@@ -64,7 +72,7 @@ export default [
     screenshot: "/assets/bee_solver_screenshot.png",
   },
   {
-    key: "9",
+    key: "2",
     name: "Notes",
     description:
       "This app is more or less a clone of simple notetaking apps like Simplenote. I made it to help me keep track of my notes while learning Flutter. It's made with Flutter, with a Google Sheets backend.",
@@ -72,7 +80,7 @@ export default [
     screenshot: "/assets/notes_app_screenshot.png",
   },
   {
-    key: "10",
+    key: "1",
     name: "Movie Watchlist",
     description:
       "This app was one of the independent exercises in the Scrimba Frontend Developer path. It's made with HTML, CSS, and JavaScript, accessing a public API.",
