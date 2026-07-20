03bc8ca3464bce7c2356ec2c2e1342440fbd0732
acolburn <acolburn3@gmail.com>
2026-07-20 14:46:51 -0700

    Added Shut the Box

--- src/myApps.js
+++ src/myApps.js
@@ -1,7 +1,22 @@
 export default [
-
+  {
+    key: "17",
+    name: "Sequence",
+    description:
+      "An online two-player version of the classic game. It's made with HTML, CSS, and JavaScript, with a Firebase realtime database backend.",
+    url: "https://acolburn.github.io/sequence-js",
+    screenshot: "/sequence_screenshot.png",
+  },
   {
     key: "16",
+    name: "Reading Log",
+    description:
+      "I use this app to keep track of my reading. It's made with HTML, CSS, and JavaScript, with a Firebase realtime database backend. Book information is gathered from the Google Books API.",
+    url: "https://acolburn.github.io/reading-log",
+    screenshot: "/books_screenshot.png",
+  },
+  {
+    key: "15",
     name: "Habit Tracker",
     description:
       "Track daily habits, keep your streaks alive, recognize extra effort days. Made with React and Tailwind CSS.",
@@ -9,24 +24,7 @@ export default [
     screenshot: "/habit_tracker_screenshot.png",
   },
   {
-    key: "15",
-    name: "Al's Weather App",
-    description:
-      "It's sort of a rule that you gotta make a weather app at some point. I made mine experimenting with 'vibe coding'.",
-    url: "https://alancolburn.com/weather",
-    screenshot: "/weather_screenshot.png",
-  },
-  {
     key: "14",
-    name: "Let's Play Farkle",
-    description:
-      "It's a multiplayer version of the classic dice game, made with React, Tailwind CSS, and Firebase.",
-    url: "https://alancolburn.com/farkle-js",
-    screenshot: "/farkle_screenshot.png",
-  },
-
-  {
-    key: "13",
     name: "Schitt's Creek Hangman",
     description:
       "Hangman--for Schitt's Creek fans. It's made with React and Tailwind CSS.",
@@ -35,7 +33,7 @@ export default [
   },
 
   {
-    key: "12",
+    key: "13",
     name: "Somm Claude",
     description:
       "Let Somm Claude find a great wine pairing for your next meal. It's a React app that uses AI.",
@@ -43,31 +41,49 @@ export default [
     screenshot: "/somm_claude_screenshot.png",
   },
   {
+    key: "12",
+    name: "Al's Weather App",
+    description:
+      "It's sort of a rule that you gotta make a weather app at some point. I made mine experimenting with 'vibe coding'.",
+    url: "https://alancolburn.com/weather",
+    screenshot: "/weather_screenshot.png",
+  },
+  {
     key: "11",
+    name: "Shut the Box",
+    description: "The classic kid's game--and my first TypeScript project!",
+    url: "https://alancolburn.com/shut-the-box",
+    screenshot: "/shut_the_box_screenshot.png",
+  },
+
+  {
+    key: "10",
+    name: "Let's Play Farkle",
+    description:
+      "It's a multiplayer version of the classic dice game, made with React, Tailwind CSS, and Firebase.",
+    url: "https://alancolburn.com/farkle-js",
+    screenshot: "/farkle_screenshot.png",
+  },
+  {
+    key: "9",
+    name: "Bee Solver",
+    description:
+      "This app helps users solve the NY Times Spelling Bee game. It's made with Anvil Python.",
+    url: "https://tinyurl.com/AlsBeeSolver",
+    screenshot: "/bee_solver_screenshot.png",
+  },
+
+  {
+    key: "8",
     name: "Chef Claude",
     description:
       "Give Chef Claude a list of ingredients you have on hand, and he'll suggest a recipe. It's a React app that uses AI.",
     url: "https://alschefclaude.netlify.app",
     screenshot: "/chef_claude_screenshot.png",
   },
+
   {
-    key: "10",
-    name: "Sequence",
-    description:
-      "An online two-player version of the classic game. It's made with HTML, CSS, and JavaScript, with a Firebase realtime database backend.",
-    url: "https://acolburn.github.io/sequence-js",
-    screenshot: "/sequence_screenshot.png",
-  },
-  {
-    key: "9",
-    name: "Reading Log",
-    description:
-      "I use this app to keep track of my reading. It's made with HTML, CSS, and JavaScript, with a Firebase realtime database backend. Book information is gathered from the Google Books API.",
-    url: "https://acolburn.github.io/reading-log",
-    screenshot: "/books_screenshot.png",
-  },
-  {
-    key: "8",
+    key: "7",
     name: "Reading Speed",
     description:
       "This app lets users calculate how fast they're reading (paper books). It's made with HTML, CSS, and JavaScript.",
@@ -75,7 +91,7 @@ export default [
     screenshot: "/reading_speed_screenshot.png",
   },
   {
-    key: "7",
+    key: "6",
     name: "Boggle",
     description:
       "This app presents a Boggle game board that lets multiple people play together on the web. It creates new boards, keeps track of time, and ends the game when time is up. Players keep track of their words and scores. It's made with Flutter.",
@@ -83,7 +99,7 @@ export default [
     screenshot: "/boggle1_screenshot.png",
   },
   {
-    key: "6",
+    key: "5",
     name: "Another Boggle Game",
     description:
       "This version of Boggle allows multiple people to play the same board, at different times. Unlike the previous version, players enter their words online and the game takes care of scoring. It's made with Anvil Python.",
@@ -91,21 +107,14 @@ export default [
     screenshot: "/boggle2_screenshot.png",
   },
   {
-    key: "5",
+    key: "4",
     name: "tv Wordle",
     description:
       "This app presents a Wordle game where every word is the name of a character in a TV show Laura and I have watched. It's made with Flutter.",
     url: "https://acolburn.github.io/wordle",
     screenshot: "/tv_wordle_screenshot.png",
   },
-  {
-    key: "4",
-    name: "Bee Solver",
-    description:
-      "This app helps users solve the NY Times Spelling Bee game. It's made with Anvil Python.",
-    url: "https://tinyurl.com/AlsBeeSolver",
-    screenshot: "/bee_solver_screenshot.png",
-  },
+
   {
     key: "3",
     name: "Notes",
@@ -114,7 +123,7 @@ export default [
     url: "https://acolburn.github.io/notes-web/",
     screenshot: "/notes_app_screenshot.png",
   },
-    {
+  {
     key: "2",
     name: "Toothbrush Timer",
     description:
